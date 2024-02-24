a90c01659da8205ed3e8a60b4c3cfe0eed42b2c3
Jack <jackgooday2@gmail.com>
2024-02-24 16:11:29 +0000

    Update Find-DomainInfo.ps1

--- Private/Find-DomainInfo.ps1
+++ Private/Find-DomainInfo.ps1
@@ -1,5 +1,5 @@
 function Find-DomainInfo {
-    <#
+  <#
   .SYNOPSIS
   Searches LDAP for generic domain information. This information is not 'vulnerabiltiies',
   but is used to build an overview of the domain and ways to make it more effecient (best practice). 
@@ -15,9 +15,9 @@ function Find-DomainInfo {
   #Add mandatory domain parameter
   [CmdletBinding()]
   Param(
-      [Parameter(Mandatory=$true)]
-      [String]
-      $Domain
+    [Parameter(Mandatory = $true)]
+    [String]
+    $Domain
   )
 
   Write-Host '[*] Extracting Domain information..' -ForegroundColor Yellow
@@ -28,47 +28,50 @@ function Find-DomainInfo {
 
 
   #Domain name
-  (Get-WmiObject Win32_ComputerSystem).Domain       #quick wmi way
-  Get-ADDomain # gets forest, domain controller, dns hostnae, etc - more info
+  $domain = Get-ADDomain # gets forest, domain controller, dns hostnae, etc - more info
 
   #Trusts
-  Get-ADTrust -Filter {TrustedDomain -eq $Domain}
+  $trusts = Get-ADTrust -Filter { TrustedDomain -eq $Domain }
 
   #Functional level
-  (Get-ADDomain).DomainMode
+  $functionallevel = $domain.DomainMode
+
+  $domaincontrollers = $domain.ReplicaDirectoryServers
 
 
   #Users
-  (Get-ADUser -Filter *).Count
+  $numUsers = (Get-ADUser -Filter *).Count
 
   #Groups - 48 is default
-  (Get-ADGroup -Filter *).count
+  $numGroups = (Get-ADGroup -Filter *).count
 
   #Computers
-  (Get-ADComputer -Filter *).Count
+  $numComputers = (Get-ADComputer -Filter *).Count
 
 
   #OUs 
-  $totalOU = (Get-ADOrganizationalUnit -filter *).count
-  Write-Output "There are $totalOU OUs within the domain"
+  $numOU = (Get-ADOrganizationalUnit -filter *).count
+  Write-Output "There are $numOU OUs within the domain"
 
   #OU efficiency improvements
-  $emptyOU = Get-ADOrganizationalUnit -filter * -Properties * | Select-Object DistinguishedName, @{Name="Length"; e={$_.DistinguishedName.length}}, Name, @{Name="numObject"; 
-  Expression = { Get-ADObject -filter * -SearchBase $_.DistinguishedName | Where-Object {$_.objectclass -ne "organizationalunit"} | Measure-Object | Select-Object -ExpandProperty Count }} | Where-Object {$_.numObject -eq 0} | Sort-Object -Property Length -Descending 
+  $emptyOU = Get-ADOrganizationalUnit -filter * -Properties * | Select-Object DistinguishedName, @{Name = "Length"; e = { $_.DistinguishedName.length } }, Name, @{Name = "numObject"; 
+    Expression                                                                                                                                                          = { Get-ADObject -filter * -SearchBase $_.DistinguishedName | Where-Object { $_.objectclass -ne "organizationalunit" } | Measure-Object | Select-Object -ExpandProperty Count }
+  } | Where-Object { $_.numObject -eq 0 } | Sort-Object -Property Length -Descending 
   $totalempty = $emptyOU.count
 
   if ($totalempty -eq 0) {
     Write-Output "There are no empty OUs!"
     return
-  } else {
+  }
+  else {
     Write-Output "There are $totalempty empty OUs within the domain"
     Write-Output "The domain size can be reduced by approximately $([math]::Round(($totalempty / $totalOU * 100), 2))%"
   }
 
 
   #GPOs
-  $totalGPO = (Get-GPO -All).count
-  Write-Output "There are $totalGPO GPOs within the domain"
+  $numGPO = (Get-GPO -All).count
+  Write-Output "There are $numGPO GPOs within the domain"
 
   #GPO efficiency improvements
   Write-Output "Finding total number of unlinked GPOs (may take a while) ..."
@@ -79,25 +82,56 @@ function Find-DomainInfo {
   if ($totalunlinked -eq 0) {
     Write-Output "There are no unlinked GPOs!"
     return
-  } else {
+  }
+  else {
     Write-Output "There are $totalunlinked unlinked GPOs within the domain"
   }
 
 
 
- # Ceritifcate Authority - might need ADCS module?
+  # Ceritifcate Authority - might need ADCS module?
+  $CAcomputer = (Find-ADCS -domain test.local).dnsHostName
+  $CAname = $CAcomputer.DisplayName
+ 
+  # Certificate Templates
+  $CAtemplates = (Find-ADCS -domain test.local).certificatetemplates
+
+  #Create object
+  $Domaininfo = [pscustomobject]@{
+    Domain                 = $Domain
+    Users                  = $numUsers
+    Groups                 = $numGroups
+    Computers              = $numComputers
+    Trusts                 = $trusts
+    FunctionalLevel        = $functionallevel
+    DomainControllers      = $domaincontrollers
+    OUs                    = $numOU
+    GPOs                   = $numGPO
+    $CertificateAuthority  = $CAcomputer, $CAname
+    $CertificiateTemplates = $CAtemplates
+  }
+  $DomainInfo
 
 
- # Certificate Templates
+  #Empty OU issue
 
 
 
+  #Unlinked GPO issue
 
-
-  #Search searchbase for descriptions from user / computer accounts 
-  Get-ADObject -SearchBase $searchBase -LDAPFilter '(&(|(objectClass=user)(objectClass=computer))(description=*))' -properties * |
-  Select-Object SamAccountName, Description
-
-  #Send to generative AI for analysis
 
 }
+
+
+<#• 1 Domain – test.local
+• X Users
+• X Groups
+• X Computers
+• X Certificate Authority – CA.test.local, CN=CA-01-CA
+• X Certificate Templates
+• X OUs, Y empty
+• X GPOs, Z unlinked
+• X Trusts
+• X Functional Level
+• X Domain Controllers
+#>
