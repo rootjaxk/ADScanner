b1fc980e5640591dbce5aba0b34bac3d484d9a6e
Jack <jackgooday2@gmail.com>
2024-02-26 11:46:15 +0000

    Update Find-DomainInfo.ps1

--- Private/Find-DomainInfo.ps1
+++ Private/Find-DomainInfo.ps1
@@ -1,8 +1,7 @@
 function Find-DomainInfo {
   <#
   .SYNOPSIS
-  Searches LDAP for generic domain information. This information is not 'vulnerabiltiies',
-  but is used to build an overview of the domain and ways to make it more effecient (best practice). 
+  Searches LDAP for generic domain information. This information is not 'vulnerabiltiies', but is used to build an overview of the domain and ways to make it more effecient (best practice). 
 
   .PARAMETER Domain
   The domain to run against, in case of a multi-domain environment
@@ -20,118 +19,119 @@ function Find-DomainInfo {
     $Domain
   )
 
-  Write-Host '[*] Extracting Domain information..' -ForegroundColor Yellow
+  Write-Host '[*] Extracting Domain information...' -ForegroundColor Yellow
   
   #Dynamically produce searchbase from domain parameter
   $SearchBaseComponents = $Domain.Split('.') | ForEach-Object { "DC=$_" }
   $searchBase = $SearchBaseComponents -join ','
 
-
   #Domain name
-  $domain = Get-ADDomain # gets forest, domain controller, dns hostnae, etc - more info
+  $domainname = Get-ADDomain # gets forest, domain 
 
   #Trusts
   $trusts = Get-ADTrust -Filter { TrustedDomain -eq $Domain }
+  if ($null -eq $trusts) {
+    $trusts = "None"
+  }
 
   #Functional level
-  $functionallevel = $domain.DomainMode
+  $functionallevel = $domainname.DomainMode
 
-  $domaincontrollers = $domain.ReplicaDirectoryServers
-
+  #Domain controllers
+  $domaincontrollers = $domainname.ReplicaDirectoryServers
 
   #Users
-  $numUsers = (Get-ADUser -Filter *).Count
+  $numUsers = (Get-ADUser -SearchBase $searchBase -Filter *).Count
 
   #Groups - 48 is default
-  $numGroups = (Get-ADGroup -Filter *).count
+  $numGroups = (Get-ADGroup -SearchBase $searchBase -Filter *).Count
 
   #Computers
-  $numComputers = (Get-ADComputer -Filter *).Count
-
+  $numComputers = (Get-ADComputer -SearchBase $searchBase -Filter *).Count
 
   #OUs 
-  $numOU = (Get-ADOrganizationalUnit -filter *).count
-  Write-Output "There are $numOU OUs within the domain"
-
-  #OU efficiency improvements
-  $emptyOU = Get-ADOrganizationalUnit -filter * -Properties * | Select-Object DistinguishedName, @{Name = "Length"; e = { $_.DistinguishedName.length } }, Name, @{Name = "numObject"; 
-    Expression                                                                                                                                                          = { Get-ADObject -filter * -SearchBase $_.DistinguishedName | Where-Object { $_.objectclass -ne "organizationalunit" } | Measure-Object | Select-Object -ExpandProperty Count }
-  } | Where-Object { $_.numObject -eq 0 } | Sort-Object -Property Length -Descending 
-  $totalempty = $emptyOU.count
-
-  if ($totalempty -eq 0) {
-    Write-Output "There are no empty OUs!"
-    return
-  }
-  else {
-    Write-Output "There are $totalempty empty OUs within the domain"
-    Write-Output "The domain size can be reduced by approximately $([math]::Round(($totalempty / $totalOU * 100), 2))%"
-  }
-
+  $numOU = (Get-ADOrganizationalUnit -SearchBase $searchBase -filter *).Count
 
   #GPOs
-  $numGPO = (Get-GPO -All).count
-  Write-Output "There are $numGPO GPOs within the domain"
+  $numGPO = (Get-GPO -All).Count
 
-  #GPO efficiency improvements
-  Write-Output "Finding total number of unlinked GPOs (may take a while) ..."
-  $GPOs = Get-GPO -All | Where-Object { $_ | Get-GPOReport -ReportType XML | Select-String -NotMatch "<LinksTo>" }
-  $totalunlinked = $GPOs.count
-
-  # Check if there are no unlinked GPOs (if script already executed)
-  if ($totalunlinked -eq 0) {
-    Write-Output "There are no unlinked GPOs!"
-    return
+  # Ceritifcate Authority
+  $adcsinfo = Find-ADCS -domain $domain
+  if ($null -eq $adcsinfo) {
+    $CAcomputer = "None"
+    $CAname = "None"
+    $numCAtemplates = "None"
+    $CAtemplates = "None"
   }
   else {
-    Write-Output "There are $totalunlinked unlinked GPOs within the domain"
+    $CAcomputer = $adcsinfo.dnsHostName
+    $CAname = $adcsinfo.DisplayName
+
+    # Certificate Templates
+    $numCAtemplates = $adcsinfo.certificateTemplates.count
+    $CAtemplates = $adcsinfo.certificatetemplates
   }
-
-
-
-  # Ceritifcate Authority - might need ADCS module?
-  $CAcomputer = (Find-ADCS -domain test.local).dnsHostName
-  $CAname = $CAcomputer.DisplayName
  
-  # Certificate Templates
-  $CAtemplates = (Find-ADCS -domain test.local).certificatetemplates
-
   #Create object
   $Domaininfo = [pscustomobject]@{
-    Domain                 = $Domain
-    Users                  = $numUsers
-    Groups                 = $numGroups
-    Computers              = $numComputers
-    Trusts                 = $trusts
-    FunctionalLevel        = $functionallevel
-    DomainControllers      = $domaincontrollers
-    OUs                    = $numOU
-    GPOs                   = $numGPO
-    $CertificateAuthority  = $CAcomputer, $CAname
-    $CertificiateTemplates = $CAtemplates
+    Domain                = $domain
+    FunctionalLevel       = $functionallevel
+    DomainControllers     = $domaincontrollers
+    Users                 = $numUsers
+    Groups                = $numGroups
+    Computers             = $numComputers
+    Trusts                = $trusts
+    OUs                   = $numOU
+    GPOs                  = $numGPO
+    CertificateAuthority  = $CAcomputer, $CAname
+    CAtemplates           = $numCAtemplates
+    CertificiateTemplates = $CAtemplates
   }
   $DomainInfo
 
 
   #Empty OU issue
+  Write-Host '[*] Finding empty OUs...' -ForegroundColor Yellow
+  $emptyOU = Get-ADOrganizationalUnit -filter * -Properties * | Select-Object DistinguishedName, @{Name = "Length"; e = { $_.DistinguishedName.length } }, Name, @{Name = "numObject"; 
+    Expression = { Get-ADObject -filter * -SearchBase $_.DistinguishedName | Where-Object { $_.objectclass -ne "organizationalunit" } | Measure-Object | Select-Object -ExpandProperty Count }
+  } | Where-Object { $_.numObject -eq 0 } | Sort-Object -Property Length -Descending 
 
+  if ($emptyOU) {
+    $Issue = [pscustomobject]@{
+      Forest    = $Domain
+      EmptyOUs  = $emptyOU.count
+      Issue     = "There are $($emptyOU.count) empty OUs within the domain. The domain structure can be reduced by approximately $([math]::Round(($emptyOU.count / $numOU * 100), 2))%. Removing complexity eases administration and reduces risk of misconfigurations"
+      Technique = "Domain effiency impprovement - empty Organizational Units"
+    }
+    $Issue  
+  }
+    foreach ($empty in $emptyOU) {
+    $emptyIssue = [pscustomobject]@{
+      Forest    = $Domain
+      OU        = $empty.DistinguishedName
+      Issue     = "The OU $($empty.Name) is empty and can be removed"
+      Technique = "Domain effiency impprovement - empty Organizational Units"
+    }
+    $emptyIssue
+  }
 
 
   #Unlinked GPO issue
+  Write-Host '[*] Finding unlinked GPOs...' -ForegroundColor Yellow
+  $GPOs = Get-GPO -All | Where-Object { $_ | Get-GPOReport -ReportType XML | Select-String -NotMatch "<LinksTo>" }
+  $totalunlinked = $GPOs.count
 
-
+  #Loop through for issues
+  if ($GPOs) {
+    foreach ($GPO in $GPOs) {
+      $Issue = [pscustomobject]@{
+        Domain        = $Domain
+        Totalunlinked = $totalunlinked
+        'GPO name'    = $GPO.DisplayName
+        Issue         = "$($GPO.DisplayName) is not linked to any OUs within the domain. Unlinked GPOs are not used and can be removed to reduce complexity and reduce risk of misconfigurations"
+        Technique     = "Domain effiency impprovement - unlinked GPOs"
+      }
+      $Issue
+    }
+  }
 }
-
-
-<#• 1 Domain – test.local
-• X Users
-• X Groups
-• X Computers
-• X Certificate Authority – CA.test.local, CN=CA-01-CA
-• X Certificate Templates
-• X OUs, Y empty
-• X GPOs, Z unlinked
-• X Trusts
-• X Functional Level
-• X Domain Controllers
-#>
